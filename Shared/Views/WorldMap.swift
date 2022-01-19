5fc2340c8756edcf62a729adb751458d05a3d278
Abdul Khan <akhan@lcs.on.ca>
2022-01-19 09:57:35 -0500

    Added tips and map view, and a tab bar to switch between

---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Abdul-CS on 2022-01-19.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, TipsList view")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}

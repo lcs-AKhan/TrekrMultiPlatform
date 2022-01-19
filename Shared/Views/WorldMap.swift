c5e28cdb3772512069ad1d246bcf21796e9696a8
Abdul Khan <akhan@lcs.on.ca>
2022-01-19 10:03:55 -0500

    Got the map view centred on North America Eastern seaboard

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -5,16 +5,25 @@
 //  Created by Abdul-CS on 2022-01-19.
 //
 
+import MapKit
 import SwiftUI
 
 struct WorldMap: View {
+    
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571), span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    
     var body: some View {
-        Text("Hello, TipsList view")
+        
+        Map(coordinateRegion: $region)
+            .navigationTitle("Map")
+        
     }
 }
 
 struct WorldMap_Previews: PreviewProvider {
     static var previews: some View {
-        WorldMap()
+        NavigationView {
+            WorldMap()
+        }
     }
 }
